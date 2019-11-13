ba8b0ff7abc1c11a67af525c0edb758139aae4ea
Jamila Aliyeva <jamila@>
2019-11-12 16:41:20 -0800

    journal template

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import { Entry } from "src/journal.js";
+
+
+$(document).ready(function() {
+  $("formOne").submit(function(event) {
+    event.preventDefault();
+
+    var journalInput = $("input#journalInput").val().split(" ");
+    var titleInput = $("input#titleInput").val();
+    var journalEntry = new Entry(titleInput, journalInput);
+
+    journalEntry.countVowels(journalInput);
+    journalEntry.countConsonants(journalInput);
+    journalEntry.getTeaser(journalInput);
+
+    $("#titleDisplay").text("Hello");
+    $("#entryDisplay").text("Hello");
+  });
+});
